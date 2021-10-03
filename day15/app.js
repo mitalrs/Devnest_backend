eca49b0a840ca4e852d346a426163299c4aacfa1
Mital Sapkale <sapkalemital@gmail.com>
2021-10-03 18:56:24 +0530

    done

--- day15/app.js
+++ day15/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/product',require("./routes/productsq"))
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
